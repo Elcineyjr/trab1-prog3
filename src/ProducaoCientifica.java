70eb566c4caf1b39328dabaa8a5d7813879afff9
Mateus Antônio Pereira Gomes <mateus_antonio3@hotmail.com>
2018-05-16 09:05:02 -0300

    base de Produção Cientifica testada

--- src/ProducaoCientifica.java
+++ src/ProducaoCientifica.java
@@ -1,5 +1,33 @@
 
 
 public class ProducaoCientifica {
+	private int codigoDoDocente;
+	private String tituloDaPublicacao;
+	private boolean qualificada;
+	
+	public ProducaoCientifica(int codigoDoDocente, String tituloDaPublicacao, boolean qualificada) {
+		super();
+		this.codigoDoDocente = codigoDoDocente;
+		this.tituloDaPublicacao = tituloDaPublicacao;
+		this.qualificada = qualificada;
+	}
+	
+	public int getCodigoDoDocente() {
+		return codigoDoDocente;
+	}
+	public String getTituloDaPublicacao() {
+		return tituloDaPublicacao;
+	}
+	public boolean isQualificada() {
+		return qualificada;
+	}
 
+	@Override
+	public String toString() {
+		return "ProducaoCientifica [codigoDoDocente=" + codigoDoDocente + ", tituloDaPublicacao=" + tituloDaPublicacao
+				+ ", qualificada=" + qualificada + "]";
+	}
+	
+	
+	
 }
