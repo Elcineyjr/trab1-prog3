d765d38c6cd156dee72ed2f68e2416277ba120aa
Elciney M R Júnior <elcineyjr123@gmail.com>
2018-05-16 10:36:58 -0300

    Base de Atividade orientada discente pos graduaçao (nao testada)

--- src/AtividadeOrientadaDiscentePosGraduacao.java
+++ src/AtividadeOrientadaDiscentePosGraduacao.java
@@ -1,4 +1,48 @@
+import java.util.Date;
 
 public class AtividadeOrientadaDiscentePosGraduacao {
+	private int codigoDocente;
+	private int matriculaDiscente;
+	private Date dataIngressoDiscente;
+	private String programa;
+	private int cargaHorariaSemanal;
+	
+	
+	public AtividadeOrientadaDiscentePosGraduacao(int codigoDocente, int matriculaDiscente, Date dataIngressoDiscente,
+			String programa, int cargaHorariaSemanal) {
+		this.codigoDocente = codigoDocente;
+		this.matriculaDiscente = matriculaDiscente;
+		this.dataIngressoDiscente = dataIngressoDiscente;
+		this.programa = programa;
+		this.cargaHorariaSemanal = cargaHorariaSemanal;
+	}
 
+	public int getCodigoDocente() {
+		return codigoDocente;
+	}
+	
+	public int getMatriculaDiscente() {
+		return matriculaDiscente;
+	}
+	
+	public Date getDataIngressoDiscente() {
+		return dataIngressoDiscente;
+	}
+	
+	public String getPrograma() {
+		return programa;
+	}
+	
+	public int getCargaHorariaSemanal() {
+		return cargaHorariaSemanal;
+	}
+
+	@Override
+	public String toString() {
+		return "AtividadeOrientadaDiscentePosGraduacao [codigoDocente=" + codigoDocente + ", matriculaDiscente="
+				+ matriculaDiscente + ", dataIngressoDiscente=" + dataIngressoDiscente + ", programa=" + programa
+				+ ", cargaHorariaSemanal=" + cargaHorariaSemanal + "]";
+	}
+	
+	
 }
