c6db3cd006046e5045a8f38886199f3de191ea12
Mateus Antônio Pereira Gomes <mateus_antonio3@hotmail.com>
2018-05-16 09:53:47 -0300

    Bases de Docente e Discente testadas

--- src/Discente.java
+++ src/Discente.java
@@ -1,5 +1,33 @@
 
 
 public class Discente {
+	private int matricula;
+	private String nome;
+	private int codigoDoCurso;
+	
+	public Discente(int matricula, String nome, int codigoDoCurso) {
+		this.matricula = matricula;
+		this.nome = nome;
+		this.codigoDoCurso = codigoDoCurso;
+	}
 
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getCodigoDoCurso() {
+		return codigoDoCurso;
+	}
+
+	@Override
+	public String toString() {
+		return "Discente [matricula=" + matricula + ", nome=" + nome + ", codigoDoCurso=" + codigoDoCurso + "]";
+	}
+	
+	
+	
 }
--- src/Docente.java
+++ src/Docente.java
@@ -1,5 +1,33 @@
 
 
 public class Docente {
+	private int codigo;
+	private String nome;
+	private String departamento;
+	
+	public Docente(int codigo, String nome, String departamento) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.departamento = departamento;
+		
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Docente [codigo=" + codigo + ", nome=" + nome + ", departamento=" + departamento + "]";
+	}
+	
 	
 }
